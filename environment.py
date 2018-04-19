8bb3cb14e931fc8c4e5262c66d832bd0c167661d
jorge52an <jorgesolavila@gmail.com>
2018-04-19 15:46:08 -0500

    Feat: update environment
    Transform fish

--- environment.py
+++ environment.py
@@ -15,21 +15,33 @@ class GameObject:
 		if self.pos.top > 600:
 			self.pos.bottom = 0
 
+	def move2(self):
+		self.pos = self.pos.move(0, self.speed)
+		if self.pos.top > 600:
+			self.pos.bottom = 0
+
 screen = pygame.display.set_mode((1920, 1200))
 player = pygame.image.load('Assets/fish2.png').convert_alpha()
 background = pygame.image.load('Assets/background.jpg').convert()
 screen.blit(background, (0, 0))
 objects = []
-for x in range(100):
-	o = GameObject(player, randrange( 200 ), 1)
+for x in range(1):
+	o = GameObject(pygame.transform.scale( player, (200, 100) ), randrange( 200 ), 1)
+	o2 = GameObject(player, randrange( 200 ), 1)
 	objects.append(o)
+	objects.append(o2)
 while 1:
 	for event in pygame.event.get():
 		if event.type in (QUIT, KEYDOWN):
 			sys.exit()
 	for o in objects:
 		screen.blit(background, o.pos, o.pos)
+	i = 0
 	for o in objects:
-		o.move()
+		if i == 0:
+			o.move()
+		else:
+			o.move2()
 		screen.blit(o.image, o.pos)
+		i += 1
 	pygame.display.update()
