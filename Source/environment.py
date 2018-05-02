d457ad20bbdeeb68a8844f080cdad741e83a1468
jorge52an <jorgesolavila@gmail.com>
2018-05-01 21:17:21 -0500

    Feat: update environment component
    Add food points

--- Source/environment.py
+++ Source/environment.py
@@ -4,46 +4,66 @@ from random import randrange as randomRange
 import pygame
 from pygame.locals import *
 
-from individuals import Individual
+from individuals import Fish, Shark, Food
 
 screen = pygame.display.set_mode( ( 960, 600 ), pygame.RESIZABLE )
 pygame.display.set_caption( "Artificial Life" )
-player = pygame.image.load( "../Assets/fish2.png" ).convert_alpha()
-player2 = pygame.transform.scale( player, ( 100, 60 ) )
-player3 = pygame.image.load( "../Assets/shark.png" ).convert_alpha()
+
+fishImage = pygame.image.load( "../Assets/fish.png" ).convert_alpha()
+#print( fishImage.get_width(), fishImage.get_height() )
+convertedFishImage = pygame.transform.scale( fishImage, ( 100, 60 ) )
+sharkImage = pygame.image.load( "../Assets/shark.png" ).convert_alpha()
+
 background = pygame.image.load( "../Assets/background.jpg" ).convert()
 screen.blit( background, ( 0, 0 ) )
-objects = []
-individual = Individual( player )
-individual2 = Individual( player2 )
-shark = Individual( player3 )
 
-"""
-for x in range( 1 ):
-	#pygame.transform.scale( player, (200, 100) )
-	o = GameObject( player, 1 )
-	objects.append( o )
-"""
+fishTotal = 4
+sharkTotal = 3
+foodTotal = 100
+fishList = []
+sharkList = []
+food = pygame.sprite.RenderPlain()
+for index in range( fishTotal ):
+	fishList.append( Fish( 1, index + 1, fishImage ) )
+	#if index % 2 == 0:
+	#	fishList.append( Fish( 1, index + 1, fishImage ) )
+	#else:
+	#	fishList.append( Fish( 2, index + 1, convertedFishImage ) )
+
+for index in range( sharkTotal ):
+	sharkList.append( Shark( 3, index + 1, sharkImage ) )
+
+height = pygame.display.Info().current_h
+width = pygame.display.Info().current_w
+for index in range( foodTotal ):
+	food.add( Food( randomRange( width ), randomRange( 3 * height / 4, height ) ) )
+screen.blit( background, ( 0, 0 ) )
+food.draw( screen )
+
 while True:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			sys.exit()
 		if event.type == pygame.VIDEORESIZE:
 			screen = pygame.display.set_mode( ( event.w, event.h ), pygame.RESIZABLE )
-			screen.blit( background, ( 0, 0 ) )
+			food.empty()
+			height = pygame.display.Info().current_h
+			width = pygame.display.Info().current_w
+			for index in range( foodTotal ):
+				food.add( Food( randomRange( width ), randomRange( 3 * height / 4, height ) ) )
 
-	screen.blit( background, individual.position, individual.position )
-	screen.blit( background, individual2.position, individual2.position )
-	screen.blit( background, shark.position, shark.position )
-	individual.move()
-	individual2.move()
-	shark.move()
-	screen.blit( individual.image, individual.position )
-	screen.blit( individual2.image, individual2.position )
-	screen.blit( shark.image, shark.position )
-	#for o in objects:
-	#	screen.blit( background, o.position, o.position )
-	#for o in objects:
-	#	o.move()
-	#	screen.blit( o.image, o.position )
+	screen.blit( background, ( 0, 0 ) )
+	food.draw( screen )
+	for fish in fishList:
+		screen.blit( background, fish.position, fish.position )
+	for shark in sharkList:
+		screen.blit( background, shark.position, shark.position )
+
+	for fish in fishList:
+		fish.move( fishList )
+		screen.blit( fish.image, fish.position )
+	for shark in sharkList:
+		shark.move()
+		screen.blit( shark.image, shark.position )
+
 	pygame.display.update()
