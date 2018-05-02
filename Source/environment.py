5f27e11d37677217481d9cd747e2d1f8f5d51609
jorge52an <jorgesolavila@gmail.com>
2018-05-02 03:38:43 -0500

    Feat: update environment component
    Add draw tree

--- Source/environment.py
+++ Source/environment.py
@@ -1,10 +1,31 @@
 import sys
 from random import randrange as randomRange
+from math import *
 
 import pygame
 from pygame.locals import *
 
 from individuals import Fish, Shark, Food, Badge
+
+def getCompleteRule( iterations ):
+	rules = ["X"]
+
+	for i in range( iterations ):
+		j = 0
+		while j < len( rules ):
+			if rules[j] == "X":
+				rules.pop( j )
+				for item in treeRules[0][::-1]:
+					rules.insert( j, item )
+				j += len( treeRules[0] ) - 1
+			elif rules[j] == "F":
+				rules.pop( j )
+				for item in treeRules[1][::-1]:
+					rules.insert( j, item )
+				j += len( treeRules[1] ) - 1
+			j += 1
+
+	return rules
 
 pygame.init()
 screen = pygame.display.set_mode( ( 960, 600 ), pygame.RESIZABLE )
@@ -18,6 +39,7 @@ sharkImage = pygame.image.load( "../Assets/shark.png" ).convert_alpha()
 background = pygame.image.load( "../Assets/background.jpg" ).convert()
 screen.blit( background, ( 0, 0 ) )
 
+treeTotal = 10
 fishTotal = 50
 sharkTotal = 3
 foodTotal = 100
@@ -26,6 +48,8 @@ sharkList = []
 food = pygame.sprite.RenderPlain()
 #badges = pygame.sprite.RenderPlain()
 font = pygame.font.SysFont( "Arial", 20, True )
+
+treeRules = ["F-[[X]+X]+F[+FX]-X", "FF"]
 
 for index in range( fishTotal ):
 	#fishList.append( Fish( 1, index + 1, fishImage ) )
@@ -58,6 +82,35 @@ while True:
 			width = pygame.display.Info().current_w
 			for index in range( foodTotal ):
 				food.add( Food( randomRange( width ), randomRange( 3 * height / 4, height ) ) )
+
+	"""rule = getCompleteRule( 5 )
+	position = ( randomRange( height ), height )
+	unit = 2.25
+	initialAngle = -90
+	angle = 20
+	angles = []
+	positions = []
+	for letter in rule:
+		if letter == "F":
+			dx = cos( radians( initialAngle ) ) * unit
+			dy = sin( radians( initialAngle ) ) * unit
+
+			initialAngleAbs = abs( initialAngle )
+			color = ( 255, 0, 0 )
+			pygame.draw.line( screen, color, position, ( position[0] + dx, position[1] + dy ) )
+			pygame.display.update( ( position[0] - 1, position[1] - 1, position[0] + dx + 1, position[1] + dy + 1 ) )
+
+			position = ( position[0] + dx, position[1] + dy )
+		elif letter == "+":
+			initialAngle += angle
+		elif letter == "-":
+			initialAngle -= angle
+		elif letter == "[":
+			positions.append( position )
+			angles.append( initialAngle )
+		elif letter == "]":
+			position = positions.pop()
+			initialAngle = angles.pop()"""
 
 	screen.blit( background, ( 0, 0 ) )
 	food.draw( screen )
