742fa0b390f10f0f666bef7fbb70c34a84a7cf2e
jorge52an <jorgesolavila@gmail.com>
2018-05-04 02:29:37 -0500

    Feat: update environment module
    Show trees

--- Source/environment.py
+++ Source/environment.py
@@ -28,6 +28,7 @@ def getCompleteRule( iterations ):
 	return rules
 
 pygame.init()
+#clock = pygame.time.Clock()
 screen = pygame.display.set_mode( ( 960, 600 ), pygame.RESIZABLE )
 pygame.display.set_caption( "Artificial Life" )
 
@@ -39,6 +40,12 @@ skin1 = pygame.image.load( "../Assets/skin1.png" ).convert()
 skin1 = pygame.transform.scale( skin1, ( int( skin1.get_width() / 4 ), int( skin1.get_height() / 4 ) ) )
 skin2 = pygame.image.load( "../Assets/skin2.png" ).convert()
 skin2 = pygame.transform.scale( skin2, ( int( skin2.get_width() / 6 ), int( skin2.get_height() / 6 ) ) )
+tree1 = pygame.image.load( "../Assets/tree1.png" ).convert_alpha()
+tree1 = pygame.transform.scale( tree1, ( int( tree1.get_width() / 2 ), int( tree1.get_height() / 2 ) ) )
+tree2 = pygame.image.load( "../Assets/tree2.png" ).convert_alpha()
+tree2 = pygame.transform.scale( tree2, ( int( tree2.get_width() / 2 ), int( tree2.get_height() / 2 ) ) )
+tree3 = pygame.image.load( "../Assets/tree3.png" ).convert_alpha()
+tree3 = pygame.transform.scale( tree3, ( int( tree3.get_width() / 2 ), int( tree3.get_height() / 2 ) ) )
 
 background = pygame.image.load( "../Assets/background.jpg" ).convert()
 screen.blit( background, ( 0, 0 ) )
@@ -79,6 +86,46 @@ screen.blit( background, ( 0, 0 ) )
 food.draw( screen )
 #badges.draw( screen )
 
+def drawTree( width, height ):
+	rule = getCompleteRule( 5 )
+	position = ( randomRange( height ), height )
+	unit = 6
+	initialAngle = -90
+	angle = 20
+	angles = []
+	positions = []
+	for letter in rule:
+		if letter == "F":
+			dx = cos( radians( initialAngle ) ) * unit
+			dy = sin( radians( initialAngle ) ) * unit
+
+			initialAngleAbs = abs( initialAngle )
+			color = ( 255, 0, 0 )
+			test1 = pygame.draw.line( screen, color, position, ( position[0] + dx, position[1] + dy ) )
+			test2 = pygame.display.update( ( position[0] - 1, position[1] - 1, position[0] + dx + 1, position[1] + dy + 1 ) )
+
+			position = ( position[0] + dx, position[1] + dy )
+		elif letter == "+":
+			initialAngle += angle
+		elif letter == "-":
+			initialAngle -= angle
+		elif letter == "[":
+			positions.append( position )
+			angles.append( initialAngle )
+		elif letter == "]":
+			position = positions.pop()
+			initialAngle = angles.pop()
+
+	return test1
+
+#tree = drawTree( width, height )
+#print( tree.center )
+
+treeImages = [tree1, tree3]
+trees = []
+for i in range( treeTotal ):
+	trees.append( treeImages[randomRange( len( treeImages ) )] )
+
 while True:
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -91,34 +138,8 @@ while True:
 			for index in range( foodTotal ):
 				food.add( Food( randomRange( width ), randomRange( 3 * height / 4, height ) ) )
 
-	"""rule = getCompleteRule( 5 )
-	position = ( randomRange( height ), height )
-	unit = 2.25
-	initialAngle = -90
-	angle = 20
-	angles = []
-	positions = []
-	for letter in rule:
-		if letter == "F":
-			dx = cos( radians( initialAngle ) ) * unit
-			dy = sin( radians( initialAngle ) ) * unit
-
-			initialAngleAbs = abs( initialAngle )
-			color = ( 255, 0, 0 )
-			pygame.draw.line( screen, color, position, ( position[0] + dx, position[1] + dy ) )
-			pygame.display.update( ( position[0] - 1, position[1] - 1, position[0] + dx + 1, position[1] + dy + 1 ) )
-
-			position = ( position[0] + dx, position[1] + dy )
-		elif letter == "+":
-			initialAngle += angle
-		elif letter == "-":
-			initialAngle -= angle
-		elif letter == "[":
-			positions.append( position )
-			angles.append( initialAngle )
-		elif letter == "]":
-			position = positions.pop()
-			initialAngle = angles.pop()"""
+	#msElapsed = clock.tick(120)
+	#drawTree( width, height )
 
 	screen.blit( background, ( 0, 0 ) )
 	food.draw( screen )
@@ -143,5 +164,8 @@ while True:
 			#badges.add( Badge( fish.position.centerx, fish.position.centery ) )
 			screen.blit( font.render( str( fish.energy ), True, ( 0, 0, 0 ) ), fish.position )
 	#badges.draw( screen )
+	for i in range( treeTotal ):
+		tree = trees[i]
+		screen.blit( tree, ( int( ( i + 1 ) * width / treeTotal ), height - tree.get_height() ) )
 
 	pygame.display.update()
