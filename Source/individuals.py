25de7e5205a2e48cf747f498dea1dab18687f7cc
jorge52an <jorgesolavila@gmail.com>
2018-05-20 22:27:09 -0500

    Feat: update individuals component
    Add Flock class

--- Source/individuals.py
+++ Source/individuals.py
@@ -1,4 +1,4 @@
-from random import randrange as randomRange
+from random import randrange as randomRange, random
 import math
 
 import pygame
@@ -6,146 +6,157 @@ import pygame.gfxdraw
 from pygame.locals import *
 from pygame.color import *
 
-SPEED = 1
-EAT = 10
-DIE = 10000
+ENERGY = 100
+VISION = 10
+MAX_VELOCITY = 4
 
-class Individual:
-	def __init__( self, type, id, image ):
+class Flock():
+	def __init__( self ):
+		self.boids = []
+
+	def addBoid( self, boid ):
+		self.boids.append( boid )
+
+	def run( self, screen, height, width ):
+		for boid in self.boids:
+			closeBoids = []
+			for otherBoid in self.boids:
+				if otherBoid.id == boid.id:
+					continue
+
+				distance = boid.distance( otherBoid )
+				if distance < 200:
+					closeBoids.append(otherBoid)
+
+			boid.moveCloser( closeBoids )
+			boid.moveWith( closeBoids )
+			boid.moveAway( closeBoids, 20 )
+
+			border = 25
+			if boid.x < border and boid.velocityX < 0:
+				boid.velocityX = -boid.velocityX * random()
+			if boid.x > width - border and boid.velocityX > 0:
+				boid.velocityX = -boid.velocityX * random()
+			if boid.y < border and boid.velocityY < 0:
+				boid.velocityY = -boid.velocityY * random()
+			if boid.y > height - border and boid.velocityY > 0:
+				boid.velocityY = -boid.velocityY * random()
+				
+			boid.move()
+			
+		for boid in self.boids:
+			boidRect = pygame.Rect( boid.rect )
+			boidRect.x = boid.x
+			boidRect.y = boid.y
+			screen.blit( boid.image, boidRect )
+
+class Individual():
+	def __init__( self, type, id, image, x, y ):
 		self.type = type
 		self.id = id
 		self.image = image
-		self.x = SPEED
-		self.y = SPEED
-		self.energy = 100
+		self.x = x
+		self.y = y
+		self.velocityX = randomRange( 1, MAX_VELOCITY ) / MAX_VELOCITY
+		self.velocityY = randomRange( 1, MAX_VELOCITY ) / MAX_VELOCITY
+		self.energy = ENERGY
+		self.vision = VISION
+		self.rect = self.image.get_rect()
 
-		height = pygame.display.Info().current_h
-		width = pygame.display.Info().current_w
+	def distance( self, individual ):
+		distanceX = self.x - individual.x
+		distanceY = self.y - individual.y
 
-		self.position = self.image.get_rect().move( randomRange( width ), randomRange( height ) )
+		return math.sqrt( distanceX * distanceX + distanceY * distanceY )
 
-	def near( self, individual ):
-		x = abs( individual.position.centerx - self.position.centerx )
-		y = abs( individual.position.centery - self.position.centery )
+	def moveCloser( self, individuals ):
+		if len( individuals ) < 1:
+			return
 
-		distance = math.sqrt( math.pow( x, 2 ) + math.pow( y, 2 ) )
+		avgX = 0
+		avgY = 0
+		for individual in individuals:
+			if individual.x == self.x and individual.y == self.y:
+				continue
 
-		return distance <= 100
+			avgX += ( self.x - individual.x )
+			avgY += ( self.y - individual.y )
 
-	def nearObject( self, position ):
-		x = abs( position[0] - self.position.centerx )
-		y = abs( position[1] - self.position.centery )
+		avgX /= len( individuals )
+		avgY /= len( individuals )
 
-		distance = math.sqrt( math.pow( x, 2 ) + math.pow( y, 2 ) )
+		distance = math.sqrt( ( avgX * avgX ) + ( avgY * avgY ) ) * -1.0
 
-		return distance <= 100
+		self.velocityX -= ( avgX / 100 )
+		self.velocityY -= ( avgY / 100 )
 
-	def movement( self, target ):
-		x = 0
-		y = 0
+	def moveWith( self, individuals ):
+		if len( individuals ) < 1:
+			return
 
-		if target.position.centerx > self.position.centerx:
-			x = SPEED * 2
-		elif target.position.centerx < self.position.centerx:
-			x = -SPEED * 2
+		avgX = 0
+		avgY = 0
 
-		if target.position.centery > self.position.centery:
-			y = SPEED * 2
-		elif target.position.centery < self.position.centery:
-			y = -SPEED * 2
+		for individual in individuals:
+			avgX += individual.velocityX
+			avgY += individual.velocityY
 
-		return ( x, y )
+		avgX /= len( individuals )
+		avgY /= len( individuals )
 
-	"""def movement( self, position ):
-		x = 0
-		y = 0
+		self.velocityX += ( avgX / 40 )
+		self.velocityY += ( avgY / 40 )
 
-		if position[0] > self.position.centerx:
-			x = 2
-		elif position[0] < self.position.centerx:
-			x = -2
+	def moveAway( self, individuals, minDistance ):
+		if len( individuals ) < 1:
+			return
 
-		if position[1] > self.position.centery:
-			y = 2
-		elif position[1] < self.position.centery:
-			y = -2
+		distanceX = 0
+		distanceY = 0
+		numClose = 0
 
-		return ( x, y )"""
+		for individual in individuals:
+			distance = self.distance( individual )
+			if  distance < minDistance:
+				numClose += 1
+				xdiff = ( self.x - individual.x )
+				ydiff = ( self.y - individual.y )
+
+				if xdiff >= 0:
+					xdiff = math.sqrt( minDistance ) - xdiff
+				elif xdiff < 0:
+					xdiff = -math.sqrt( minDistance ) - xdiff
+
+				if ydiff >= 0:
+					ydiff = math.sqrt( minDistance ) - ydiff
+				elif ydiff < 0:
+					ydiff = -math.sqrt( minDistance ) - ydiff
+
+				distanceX += xdiff
+				distanceY += ydiff
+
+		if numClose == 0:
+			return
+
+		self.velocityX -= distanceX / 5
+		self.velocityY -= distanceY / 5
+
+	def move( self ):
+		if abs( self.velocityX ) > MAX_VELOCITY or abs( self.velocityY ) > MAX_VELOCITY:
+			scaleFactor = MAX_VELOCITY / max( abs( self.velocityX ), abs( self.velocityY ) )
+			self.velocityX *= scaleFactor
+			self.velocityY *= scaleFactor
+
+		self.x += self.velocityX
+		self.y += self.velocityY
 
 class Fish( Individual ):
-	def move( self, fishList, sharkList, food ):
-		height = pygame.display.Info().current_h
-		width = pygame.display.Info().current_w
-
-		"""nearFish = []
-		for fish in fishList:
-			if self.id != fish.id and self.type == fish.type:
-				near = self.near( fish )
-				if near:
-					nearFish.append( fish )
-
-		countNearFish = len( nearFish )
-		if countNearFish > 0:
-			averageX = 0
-			averageY = 0
-			for index in range( countNearFish ):
-				averageX += nearFish[index].position.centerx
-				averageY += nearFish[index].position.centery
-			averageX /= countNearFish
-			averageY /= countNearFish
-
-			self.x, self.y = self.movement( ( averageX, averageY ) )"""
-
-		nearShark = None
-		for shark in sharkList:
-			near = self.near( shark )
-			if near:
-				nearShark = shark
-				break
-
-		if near:
-			self.x, self.y = self.movement( nearShark )
-			self.x *= -1
-			self.y *= -1
-
-		for sprite in food:
-			near = self.nearObject( ( sprite.rect.centerx, sprite.rect.centery ) )
-			if near:
-				self.energy += EAT
-
-		if self.position.left < 0:
-			self.x = SPEED
-		elif self.position.right > width:
-			self.x = -SPEED
-
-		if self.position.bottom < height / 2:
-			self.y = SPEED
-		elif self.position.top > height:
-			self.y = -SPEED
-
-		self.position = self.position.move( self.x, self.y )
+	def move( self ):
+		pass
 
 class Shark( Individual ):
-	def move( self, fishList ):
-		height = pygame.display.Info().current_h
-		width = pygame.display.Info().current_w
-
-		for fish in fishList:
-			near = self.near( fish )
-			if near:
-				fish.energy -= DIE
-
-		if self.position.left < 0:
-			self.x = 1
-		elif self.position.right > width:
-			self.x = -1
-
-		if self.position.bottom < 0:
-			self.y = 1
-		elif self.position.top > height:
-			self.y = -1
-
-		self.position = self.position.move( self.x, self.y )
+	def move( self ):
+		pass
 
 class Food( pygame.sprite.Sprite ):
 	def __init__( self, x, y ):
@@ -155,24 +166,6 @@ class Food( pygame.sprite.Sprite ):
 		self.color = "red"
 		self.width = 10
 		self.height = 10
-		self.image = self.draw()
-		self.rect = Rect( x, y, self.width, self.height )
-
-	def draw( self ):
-		screen = pygame.Surface( ( self.width, self.height ) )
-		screen.set_colorkey( ( 0, 0, 0 ) )
-		pygame.gfxdraw.filled_circle( screen, int( self.width / 2 ), int( self.height / 2 ), int( self.height / 2 - 1 ), THECOLORS[self.color] )
-
-		return screen
-
-class Badge( pygame.sprite.Sprite ):
-	def __init__( self, x, y ):
-		pygame.sprite.Sprite.__init__( self )
-		self.x = x
-		self.y = y
-		self.color = "yellow"
-		self.width = 30
-		self.height = 30
 		self.image = self.draw()
 		self.rect = Rect( x, y, self.width, self.height )
 
